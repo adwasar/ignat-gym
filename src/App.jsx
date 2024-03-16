954581c6cc3415ad151e02c0141a336deb9a99ab
Vladislav <104913853+Adwasar@users.noreply.github.com>
2024-03-16 11:01:23 +0200

    init project

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <>
+      <div>main page</div>
+      <h1>Title</h1>
+      <h2>Title</h2>
+      <h3>Title</h3>
+      <h4>Title</h4>
+    </>
+  )
+}
+
+export default App
